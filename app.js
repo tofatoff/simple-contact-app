75d95850a6aa37dd0154571c69374e839bda4a93
tofatoff <mail.mstofa@gmail.com>
2022-01-17 23:04:46 +0700

    part 2: using command line to add and validator

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 const { question, saveContact } = require("./contacts");
+const yargs = require("yargs");
 
-const main = async() => {
-    const nama = await question("Nama");
-    const noHP = await question("No HP");
-    const email = await question("Email");
+yargs.command({
+    command: "add",
+    describe: "Add new contact",
+    builder: {
+        nama: {
+            describe: "Full name",
+            demandOption: true,
+            type: "string",
+        },
+        noHP: {
+            describe: "Phone number",
+            demandOption: true,
+            type: "string",
+        },
+        email: {
+            describe: "Email address",
+            demandOption: true,
+            type: "string",
+        },
+    },
+    handler(argv) {
+        saveContact(argv.nama, argv.noHP, argv.email);
+    },
+});
 
-    saveContact(nama, noHP, email);
-};
+yargs.parse();
 
-main();
+// const main = async() => {
+//     const nama = await question("Nama");
+//     const noHP = await question("No HP");
+//     const email = await question("Email");
+
+//     saveContact(nama, noHP, email);
+// };
+
+// main();
--- contacts.js
+++ contacts.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
-
-const readline = require("readline");
+const validator = require("validator");
 
 const dirPath = "./data";
 const filePath = "./data/contacts.json";
@@ -13,28 +12,25 @@ if (!fs.existsSync(filePath)) {
     fs.writeFileSync(filePath, "[]", "utf-8");
 }
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
-const question = (q) => {
-    return new Promise((resolve, reject) => {
-        rl.question(q, (nama) => {
-            resolve(nama);
-        });
-    });
-};
-
 const saveContact = (nama, noHP, email) => {
     const contact = { nama, noHP, email };
     const fileBuffer = fs.readFileSync(filePath, "utf-8");
     const contacts = JSON.parse(fileBuffer);
 
+    if (email) {
+        if (!validator.isEmail(email)) {
+            console.log("Email is invalid");
+            return false;
+        }
+    }
+
+    if (!validator.isMobilePhone(noHP, "id-ID")) {
+        console.log("Phone is invallid");
+        return false;
+    }
     contacts.push(contact);
     fs.writeFileSync(filePath, JSON.stringify(contacts));
     console.log("data has been inputed");
-    rl.close();
 };
 
-module.exports = { question, saveContact };
+module.exports = { saveContact };
