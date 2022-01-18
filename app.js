0795770c90c475a6811f52c7bdb984f47546687d
tofatoff <mail.mstofa@gmail.com>
2022-01-18 09:53:19 +0700

    contact part 3 with slight modifications

--- app.js
+++ app.js
@@ -1,30 +1,69 @@
-const { question, saveContact } = require("./contacts");
+const { saveContact, listContact, detailContact, deleteContact } = require("./contacts");
 const yargs = require("yargs");
 
-yargs.command({
-    command: "add",
-    describe: "Add new contact",
-    builder: {
-        nama: {
-            describe: "Full name",
-            demandOption: true,
-            type: "string",
+yargs
+    .command({
+        command: "add",
+        describe: "Add new contact",
+        builder: {
+            nama: {
+                describe: "Full name",
+                demandOption: true,
+                type: "string",
+            },
+            noHP: {
+                describe: "Phone number",
+                demandOption: true,
+                type: "string",
+            },
+            email: {
+                describe: "Email address",
+                demandOption: true,
+                type: "string",
+            },
         },
-        noHP: {
-            describe: "Phone number",
-            demandOption: true,
-            type: "string",
+        handler(argv) {
+            saveContact(argv.nama, argv.noHP, argv.email);
         },
-        email: {
-            describe: "Email address",
-            demandOption: true,
-            type: "string",
+    })
+    .demandCommand();
+
+yargs
+    .command({
+        command: "list",
+        describe: "Show contacts list",
+        handler() {
+            listContact();
         },
-    },
-    handler(argv) {
-        saveContact(argv.nama, argv.noHP, argv.email);
-    },
-});
+    })
+    .demandCommand();
+
+yargs
+    .command({
+        command: "detail",
+        describe: "Show contact details by name",
+        handler(argv) {
+            detailContact(argv.nama);
+        },
+    })
+    .demandCommand();
+
+yargs
+    .command({
+        command: "delete",
+        describe: "Delete a contact by name",
+        builder: {
+            nama: {
+                describe: "Full name",
+                demandOption: true,
+                type: "string",
+            },
+        },
+        handler(argv) {
+            deleteContact(argv.nama);
+        },
+    })
+    .demandCommand();
 
 yargs.parse();
 
--- contacts.js
+++ contacts.js
@@ -12,10 +12,16 @@ if (!fs.existsSync(filePath)) {
     fs.writeFileSync(filePath, "[]", "utf-8");
 }
 
-const saveContact = (nama, noHP, email) => {
-    const contact = { nama, noHP, email };
+const loadContacts = () => {
     const fileBuffer = fs.readFileSync(filePath, "utf-8");
     const contacts = JSON.parse(fileBuffer);
+    return contacts;
+};
+
+const saveContact = (nama, noHP, email) => {
+    const contact = { nama, noHP, email };
+
+    const contacts = loadContacts();
 
     if (email) {
         if (!validator.isEmail(email)) {
@@ -33,4 +39,47 @@ const saveContact = (nama, noHP, email) => {
     console.log("data has been inputed");
 };
 
-module.exports = { saveContact };
+const listContact = () => {
+    const contacts = loadContacts();
+    console.log("List of contacts:");
+    contacts.forEach((contact, i) => {
+        console.log(`${i + 1}. ${contact.nama} - ${contact.noHP}`);
+    });
+};
+
+const detailContact = (nama) => {
+    const contacts = loadContacts();
+    const contact = contacts.find((contact) => contact.nama.toLowerCase() === nama.toLowerCase());
+
+    if (!contact) {
+        console.log(`Contact with name ${nama} isn't found`);
+        return false;
+    }
+
+    const keys = Object.keys(contact);
+    for (let i = 0; i < keys.length; i++) {
+        console.log(contact[keys[i]]);
+    }
+};
+
+const deleteContact = (nama) => {
+    const contacts = loadContacts();
+    const contact = contacts.find((contact) => contact.nama.toLowerCase() === nama.toLowerCase());
+
+    if (!contact) {
+        console.log(`Contact with name ${nama} isn't found`);
+        return false;
+    }
+
+    for (let i = 0; i < contacts.length; i++) {
+        if (contacts[i].nama === nama) {
+            contacts.splice(i, 1);
+        }
+    }
+
+    //console.log(contacts);
+    fs.writeFileSync(filePath, JSON.stringify(contacts));
+    console.log("Contact has been deleted");
+};
+
+module.exports = { saveContact, listContact, detailContact, deleteContact };
